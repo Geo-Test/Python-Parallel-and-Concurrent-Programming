9fb4a2475946c775dd03c37acfefcd6e239b521d
unknown <taghizadeh.mhmd@gmail.com>
2022-01-05 20:04:03 +0330

    Threading\S01-01-Thread-first-example

---
+++ Threading/S01-Thread/S01-01-Thread-first-example.py
@@ -0,0 +1,24 @@
+from threading import Thread
+import time
+
+
+start = time.perf_counter()
+
+def some_task(name, delay):
+    print(f"Thread {name} Started.")
+    time.sleep(delay)
+    print(f"Thread {name} Finished.")
+
+
+t1 = Thread(target=some_task, args=("One", 3, ), daemon=False) # daemon = False (by default)
+t2 = Thread(target=some_task, args=("Two", 3, ), daemon=False) # daemon = False (by default)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+end = time.perf_counter()
+exe_time = end - start
+print(f"Execution Time: {exe_time} sec")
